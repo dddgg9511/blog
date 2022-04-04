7580cb994ca00f0adda1e58699d5ad8d19977835
ddgg9511@naver.com <ddgg9511@naver.com>
2022-04-04 21:50:47 +0900

    [FEAT] PostService, PostRepository 추가

---
+++ server/src/main/java/com/choo/blog/service/posts/PostService.java
@@ -0,0 +1,35 @@
+package com.choo.blog.service.posts;
+
+import com.choo.blog.domain.posts.PostRepository;
+import com.choo.blog.domain.posts.Posts;
+import com.choo.blog.dto.posts.PostRequestData;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@Service
+public class PostService {
+    private final PostRepository postRepository;
+
+    public List<Posts> getPosts(){
+        return null;
+    }
+
+    public Posts getPost(Long id){
+        return null;
+    }
+
+    public Posts save(PostRequestData saveData){
+        return null;
+    }
+
+    public Posts update(PostRequestData updateData){
+        return null;
+    }
+
+    public Posts delete(Long id){
+        return null;
+    }
+}
